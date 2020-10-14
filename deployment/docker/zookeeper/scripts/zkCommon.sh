5277a3b489d8d67d954c46128e39393883d66d85
kaja <shawalpha5@gmail.com>
2020-10-14 09:44:55 +0000

    Fixed dockerfile build error

---
+++ deployment/docker/zookeeper/scripts/zkCommon.sh
@@ -0,0 +1,88 @@
+#!/usr/bin/env bash
+
+
+POD_NAME=`hostname -s`
+DATA_DIR=/data
+CONFIG_DIR=/data/conf
+MYID_FILE=$DATA_DIR/myid
+STATIC_CONFIG_FILE=$CONFIG_DIR/zoo.cfg
+DYNAMIC_CONFIG_FILE=$CONFIG_DIR/zoo.cfg.dynamic
+
+LOG4J_QUIET_CONF=/conf/log4j-quiet.properties
+LOG4J_CONF=/conf/log4j.properties
+
+function zkServerConfig() {
+  role=$1
+  HOST="$POD_NAME.$SERVICE_NAME"
+  echo "$HOST:$QUORUM_PORT:$LEADER_PORT:$role;$CLIENT_PORT"
+}
+
+function zkConnectionString() {
+  set +e
+  nslookup "${CLIENT_HOST}" &>/dev/null
+  if [[ $? -eq 0 ]]; then
+    set -e
+    echo "${CLIENT_HOST}:${CLIENT_PORT}"
+  else
+    retries=0
+    while [ $retries -gt 0 ]
+    do
+      sleep 2
+      echo "zkConnectionString() retry countdown: $retries" >&2
+      if [[ $(nslookup "${CLIENT_HOST}" &>/dev/null) ]]; then
+        echo "${CLIENT_HOST}:${CLIENT_PORT}"
+        return
+      fi
+      retries=$((retries - 1))
+    done
+    set -e
+    echo "zkConnectionString() failed: unable to lookup client host($CLIENT_HOST)"
+    exit 1
+  fi
+}
+
+function ensemblePresent() {
+  set +e
+  ## Check if there is already an existing ensemble
+  LOOKUP_RESULT=$(nslookup $SERVICE_NAME)
+  if [[ $? -eq 0 ]]; then
+    checkServicePort
+    return $?
+  elif echo $LOOKUP_RESULT | grep -q "server can't find $SERVICE_NAME"; then
+    echo "could not detect any existing ensemble:: $LOOKUP_RESULT ::"
+    return 1
+  else ## lookup failed; do a sleep-then retry loop for a finite time
+    retries=0
+    while [ $retries -gt 0 ]
+    do
+      sleep 2
+      retries=$((retries - 1))
+      echo "ensemblePresent() retry-countdown: $retries"
+      if [[ $(nslookup $SERVICE_NAME) -eq 0 ]]; then
+        checkServicePort
+        return $?
+      fi
+    done
+    return 1
+  fi
+}
+
+function checkServicePort() {
+  set +e
+  nc -z -w5 $SERVICE_NAME $CLIENT_PORT
+  if [[ $? -ne 0 ]]; then
+     retries=0
+     while [ $retries -gt 0 ]
+     do
+       sleep 2
+       retries=$((retries - 1))
+       echo "checkServicePort() retry-countdown: $retries" >&2
+       nc -z -w5 $SERVICE_NAME $CLIENT_PORT
+       if [[ $? -eq 0 ]]; then
+         return 0
+       fi
+     done
+     return 1
+  fi
+  return 0
+}
---
+++ deployment/docker/zookeeper/scripts/zkStart.sh
@@ -0,0 +1,107 @@
+#!/usr/bin/env bash
+
+
+source /scripts/zkCommon.sh
+
+set -ex
+
+mkdir -p $CONFIG_DIR
+
+#cat /etc/hosts
+#cat /etc/resolv.conf
+#cat /etc/hosts
+#nslookup $POD_NAME
+
+# Extract resource name and this members ordinal value from the pod's hostname
+if [[ $POD_NAME =~ (.*)-([0-9]+)$ ]]; then
+    NAME=${BASH_REMATCH[1]}
+    MYID=$((${BASH_REMATCH[2]} + 1))
+else
+    echo "bad hostname \"$POD_NAME\". Expecting to match the regex: (.*)-([0-9]+)$"
+    exit 1
+fi
+
+MYID_FILE_PRESENT=false
+DYNAMIC_CONFIG_FILE_PRESENT=false
+
+if [ -f $MYID_FILE ]; then
+  CURRENT_ID=$(cat $MYID_FILE)
+    if [[ "$CURRENT_ID" == "$MYID" && -f $STATIC_CONFIG_FILE ]]; then
+    # If Id is correct and configuration is present under `/data/conf`
+      MYID_FILE_PRESENT=true
+  fi
+fi
+
+if [ -f $DYNAMIC_CONFIG_FILE ]; then
+  DYNAMIC_CONFIG_FILE_PRESENT=true
+fi
+
+
+
+ENSEMBLE_PRESENT=false
+
+set +e
+$(ensemblePresent)
+if [[ $? -eq 0 ]]; then
+  echo "An ensemble is already existing at '$SERVICE_NAME'"
+  ENSEMBLE_PRESENT=true
+fi
+set -e
+
+ADD_NODE=false
+
+if [[ "$MYID_FILE_PRESENT" == false || "$DYNAMIC_CONFIG_FILE_PRESENT" == false ]]; then
+    echo "Node configuration is missing; writing myid: $MYID to: $MYID_FILE"
+    echo $MYID > $MYID_FILE
+    if [[ $MYID -eq 1 ]]; then
+      echo "I'm the first server in the ensemble; my dynamic config will be generated from the local template."
+      DYNAMIC_CONFIG="server.${MYID}=$(zkServerConfig participant)"
+      echo "Writing my dynamic configuration to $DYNAMIC_CONFIG_FILE"
+      echo $DYNAMIC_CONFIG
+      echo $DYNAMIC_CONFIG > $DYNAMIC_CONFIG_FILE
+    else
+      set -e
+      echo "There is already an ensemble; my dynamic config will be created from the ensemble's configuration."
+      ZK_URL=$(zkConnectionString)
+      DYNAMIC_CONFIG=$(zk-shell $ZK_URL --run-once "get /zookeeper/config" | cat | head -n -1)
+      echo "Writing my dynamic configuration to $DYNAMIC_CONFIG_FILE"
+      echo "$DYNAMIC_CONFIG"
+      echo "$DYNAMIC_CONFIG" > $DYNAMIC_CONFIG_FILE
+      set +e
+    fi
+  if [[ "$ENSEMBLE_PRESENT" == true ]]; then
+    ADD_NODE=true
+  fi
+fi
+
+if [[ "$ADD_NODE" == true ]]; then
+    set -e
+    ZK_URL=$(zkConnectionString)
+    DYNAMIC_CONFIG=$(zkServerConfig observer)
+    echo "Adding the node to the ensemble and writing it's dynamic configuration to disk."
+    DYNAMIC_CONFIG=$(zk-shell $ZK_URL --run-once "reconfig add $DYNAMIC_CONFIG" | cat | head -n -1)
+    echo "Writing my dynamic configuration to $DYNAMIC_CONFIG_FILE"
+    echo "$DYNAMIC_CONFIG"
+    echo "$DYNAMIC_CONFIG" > $DYNAMIC_CONFIG_FILE
+    set +e
+fi
+
+if [[ ! -f $STATIC_CONFIG_FILE ]]; then
+  echo "The static config file does not exists. copying /conf/zoo.cfg to $CONFIG_DIR"
+  cp -f /conf/zoo.cfg $CONFIG_DIR
+fi
+
+cp -f /conf/log4j.properties $CONFIG_DIR
+cp -f /conf/log4j-quiet.properties $CONFIG_DIR
+
+ZOOCFGDIR=$CONFIG_DIR
+export ZOOCFGDIR
+
+if [ -f $DYNAMIC_CONFIG_FILE ]; then
+  # This node was added to the ensemble
+  echo "Starting the zookeeper service: $ENSEMBLE_PRESENT"
+  zkServer.sh --config $ZOOCFGDIR start-foreground
+else
+  echo "Zookeeper node setup failed!!"
+  exit 1
+fi
