77d6fea1c779c6284d4fd0c121319b29bc48a41a
Alpha Shaw <shawalpha5@gmail.com>
2025-01-12 18:44:17 +0000

    update...

--- internal/controller/zookeepercluster/clusterstatus.go
+++ internal/controller/zookeepercluster/clusterstatus.go
@@ -33,7 +33,9 @@ func updateMetadata(ctx reconciler.Context, c *v1alpha1.ZookeeperCluster) error 
 	if *c.Spec.Size != c.Status.Metadata.Size ||
 		c.Spec.ZkConfig != c.Status.Metadata.ZkConfig ||
 		c.Spec.ZookeeperVersion != c.Status.Metadata.ZkVersion {
-		ctx.Logger().Info("Reconciling the cluster status data", "cluster", c.GetName())
+		ctx.Logger().Info("Reconciling the cluster status data",
+			"cluster", c.GetName(), "deletionTimestamp", c.DeletionTimestamp,
+			"specSize", c.Spec.Size, "specVersion", c.Spec.ZookeeperVersion, "specConfig", c.Spec.ZkConfig)
 		// Update metadata only if the cluster is not being deleted
 		if c.DeletionTimestamp.IsZero() {
 			c.Status.Metadata.Size = *c.Spec.Size
@@ -47,8 +49,8 @@ func updateMetadata(ctx reconciler.Context, c *v1alpha1.ZookeeperCluster) error 
 					return err
 				}
 			}
+			ctx.Logger().Info("Updating the cluster status", "cluster", c.GetName(), c.Status)
 			if err := ctx.Client().Status().Update(context.TODO(), c); err != nil {
-				ctx.Logger().Info("Updating the cluster status", "cluster", c.GetName())
 				return err
 			}
 		}
