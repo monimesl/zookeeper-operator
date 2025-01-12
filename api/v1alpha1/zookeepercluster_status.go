21aff1ce96d0c1849924e776a3dbcd28d35784d5
Alpha Shaw <shawalpha5@gmail.com>
2025-01-12 19:02:27 +0000

    update...

--- api/v1alpha1/zookeepercluster_status.go
+++ api/v1alpha1/zookeepercluster_status.go
@@ -33,8 +33,11 @@ type Metadata struct {
 
 // setDefaults set the defaults for the cluster status and returns true otherwise false
 func (in *ZookeeperClusterStatus) setDefaults() (changed bool) {
-	in.Metadata = Metadata{
-		Data: make(map[string]string),
+	if in.Metadata.Data == nil {
+		in.Metadata = Metadata{
+			Data: make(map[string]string),
+		}
+		changed = true
 	}
 	return
 }
